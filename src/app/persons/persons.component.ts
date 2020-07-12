46f1d6f9106136217956d17f1aeb94308b516e23
Luam Leite <luamleite@nasajon.com.br>
2020-07-12 13:38:50 -0300

    Trabalhando com lifecycle hooks

--- src/app/persons/persons.component.ts
+++ src/app/persons/persons.component.ts
@@ -2,7 +2,8 @@
 //Também adicionamos o arquivo com o mesmo nome .component.ts
 //18) Importamos a classe Component que usaremos de forma a nosso novo componente ser reconhecido
 //94) Não precisamos mais adicionar uma propriedade ligável aqui, então vamos nos livrar do Input
-import { Component} from '@angular/core'; //32) Input é um decorator, porém é um que não se declara abaixo como foi feito no @Component
+//107) Importando OnInit
+import { Component, OnInit} from '@angular/core'; //32) Input é um decorator, porém é um que não se declara abaixo como foi feito no @Component
 //100)Adicionando o Import para o construtor fucnionar
 import { PersonsService } from './persons.service';
 @Component ({ //20)Para ser tratado como um componente é necessario adicionar o @Component em todo arquivo que se for usar
@@ -13,7 +14,12 @@ import { PersonsService } from './persons.service';
   //23) Agora iremos registrar o nosso decorator, em app.module.ts
 
 })
-export class PersonsComponent //19) Nota que ao exportar Classes, usa-se letras maiusculas
+//105) Para tal, faremos nossa classe implementar uma Interface, que força nossa classe a implementar uma funcionalidade
+export class PersonsComponent implements OnInit//19) Nota que ao exportar Classes, usa-se letras maiusculas
+//106)Faremos essa classe implementar a interface OnInit, e claro, importaremos essa interface
+//108)Agora, precisamos importar o método ngOnInit para não ter erros, que é o gancho que executará ao criar nosso componente
+
+
 {
 //30) Então aqui, queremos ter persons, mas, vamos nomear de forma diferente para ficar facil de diferenciar as diferentes propriedades
 // personList: string[]; //31) Tem que definir o tipo no TypeScript. Porém a informação, a data que queremos não está aqui, está no app.component.ts, e para pegarmos, precisaremos de uma
@@ -22,14 +28,36 @@ export class PersonsComponent //19) Nota que ao exportar Classes, usa-se letras 
 //95) Nos livramos do Input como dito anteriormente
 //@Input() personList: string[]; //33) O que isso diz para o Angular é que a propriedade personList desse componente pode ser bound/ligada por fora. Agora podemos ir no app.component.html
 personList: string[];
+//)111 Nova propriedade privada, agora podemos no construtor fazer essa propriedade ficar disponivel na classe toda
+private personService: PersonsService;
 //96)Fizemos uma lista de pessoas que iremos pegar, mas, para isso, precisamos informar ao Angular que é injetável
 //97)Adicionaremos um construtor para o Angular conseguir saber tudo que esse componente precisa de injeção
 //98)Essa função interna será executada toda vez que Angular criar uma nova instancia desse componente
 constructor(prsService: PersonsService) {
   //101 Agora podemos colocar this.personList = prsService.persons, ou seja, aqui personList
   //corresponderá a prsService.persons
-  this.personList = prsService.persons;
-} //Adicionamos aqui o prsService(podemos mudar esse nome), porém é importante ao lado a declaração do tipo que precisamos
+  //103 Conseguimos usar a lista desse jeito, porém, essa não é a maneira recomendada
+  //this.personList = prsService.persons;
+  //104) Deve-se usar algo chamado gancho de ciclo de vida em um service. Cada componente do Angular passa por um ciclo de vida
+  //O ciclo consiste normalmente em:
+  /*
+  1-Angular cria
+  2-Renderiza
+  3-Cria e renderiza as childrens do objeto
+  4-Checa quando as propriedades ligadas a dados mudam
+  5-Destroi antes de remover do DOM
+  E é possível adicionar comandos específicos no Angular, de forma que ele execute em cada um desses hooks*/
+  //112) E agora faremos como deve ser feito:
+  this.personService = prsService;
+}
+//109) Criação do componente
+ngOnInit(){
+  //110)Precisariamos colocar aqui para a personList pegar do service dessa forma ->this.personList = prsService.persons;
+  //Porém prsService só fica disponivel no constructor, então criaremos uma nova propriedade privada
+  //113) Agora implementando
+  this.personList = this.personService.persons;
+}
+//Adicionamos aqui o prsService(podemos mudar esse nome), porém é importante ao lado a declaração do tipo que precisamos
 //99) E adicionar no topo o import desse tipo
 //102 agora se no app.component.html deixarmos a tag app-persons visivel, funcionará novamente
 
